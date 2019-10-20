7cde6708281cb43d045a4f2992731764baabeef0
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-20 19:20:38 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,9 +6,8 @@ class Show < ActiveRecord::Base
   end 
   
   def self.most_popular_show 
-    Show.where(rating: Show.maximum)
-    # show = Show.maximum(:rating)
-    # show.name 
+    show = Show.where(rating: highest_rating)
+    show
   end 
   
 end 
